3922c3ece372a580175e913cceeed1bffba16ca7
asaqui94 <arpitasaqui@gmail.com>
2021-02-18 09:35:53 -0500

    day-3 changes

--- src/components/Cars/Cars.tsx
+++ src/components/Cars/Cars.tsx
@@ -2,11 +2,27 @@ import React, {useState, useEffect} from 'react';
 import {useGetData} from '../../custom-hooks';
 import { Jumbotron, Button, Container, Card, Col,Row } from 'react-bootstrap';
 import Logo2 from '../../assets/img/Logo2.png';
+import {useHistory} from 'react-router-dom';
+import { server_calls } from '../../api';
 
 export const Cars = () => {
 
+    const history:any = useHistory();
+
+    const routeChange = (id?:string,path?:string) => {
+        history.push({
+            pathname:path,
+            state:{car_id:id}
+        })
+    }
+
     let {carData, getData} = useGetData();
     console.log(carData)
+
+    const handleDeleteCar = (id:any) => {
+        server_calls.delete(id);
+        getData()
+    }
 
     return (
         <Container>
@@ -15,7 +31,7 @@ export const Cars = () => {
                     <Jumbotron>
                         <h1>Hello Carpi's Members!</h1>
                         <p>Here are your current collection of cars!</p>
-                        <Button>Create New Car</Button>
+                        <Button onClick={()=>routeChange('','create')}>Create New Car</Button>
                     </Jumbotron>
                 </Col>
             </Row>
@@ -33,17 +49,17 @@ export const Cars = () => {
                                             { item.name }
                                         </Card.Title>
                                         <Card.Text>
-                                            {item.model}
+                                            { item.price }
                                         </Card.Text>
                                         <Card.Text>
-                                            { item.price }
+                                            {item.model}
                                         </Card.Text>
                                         <Card.Text>
                                             { item.year }
                                         </Card.Text>
 
-                                        <Button variant="danger">Delete</Button>
-                                        <Button variant="primary">Update</Button>
+                                        <Button variant="danger" onClick={()=>handleDeleteCar(item.id)}>Delete</Button>
+                                        <Button variant="primary" onClick={()=>routeChange(item.id,'update')}>Update</Button>
                                     </Card.Body>   
                                   </Card>      
                             </div>
--- src/components/CreateCar/CreateCar.tsx
+++ src/components/CreateCar/CreateCar.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
+import {server_calls} from '../../api';
+import {Jumbotron, Container, Col, Row} from 'react-bootstrap'
+
+type Inputs = {
+    name:string,
+    price:number,
+    model:string,
+    year:number
+}
 
 export const CreateCar = () => {
 
+    const {register, handleSubmit} = useForm<Inputs>();
+
+    const onSubmit = (data:any) => {
+        console.log(data)
+        server_calls.create(data)
+    }
+
     return (
-        <div>
+        <Container>
             <h1>Create Your New Car</h1>
-        </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="name">Car Name</label>
+                <input type="text" name="name" id="name" placeholder="Add Car Name" ref={register}/>
+                
+                <label htmlFor="price">Car Price</label>
+                <input type="text" name="price" id="price" placeholder="Add Car Price" ref={register}/>
+                
+                <label htmlFor="model">Car Model</label>
+                <input type="text" name="model" id="model" placeholder="Add Car Model" ref={register}/>
+                
+                <label htmlFor="year">Car Year</label>
+                <input type="text" name="year" id="year" placeholder="Add Car Year" ref={register}/>
+
+                <button type="submit" className="button-styles">Submit Car</button>
+            </form>
+        </Container>
     )
 }
